ce5e546c53578acf8dcc7c4c253156f91b3a1a44
Nurul Huda <nhuda55161@gmail.com>
2022-08-23 12:35:52 +0700

    Update Model

---
+++ service-course/app/Models/Chapter.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Chapter extends Model
+{
+    protected $table = 'chapters';
+
+    protected $fillable = [
+        'name', 'course_id'
+    ];
+
+    public function lessons()
+    {
+        return $this->hasMany('App\Lesson')->orderBy('id', 'ASC');
+    }
+
+
+}
---
+++ service-course/app/Models/Review.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Review extends Model
+{
+    protected $table = 'reviews';
+
+    protected $fillable = [
+        'user_id', 'course_id', 'rating', 'note'
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo('App\Course');
+    }
+}
