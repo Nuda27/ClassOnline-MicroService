72c60469f60fb890d1fb6368350e6f86fc76df34
Nurul Huda <nhuda55161@gmail.com>
2022-08-24 09:40:44 +0700

    Update CRUD API Mentor

--- service-course/app/Http/Controllers/MentorController.php
+++ service-course/app/Http/Controllers/MentorController.php
@@ -8,6 +8,28 @@ use Illuminate\Support\Facades\Validator;
 
 class MentorController extends Controller
 {
+    public function index()
+    {
+        $mentors = Mentor::all();
+        return response()->json([
+            'status' => 'Success',
+            'data' => $mentors
+         ]);
+    }
+
+    public function show($id)
+    {
+        $mentor = Mentor::find($id);
+        if(!$mentor){
+            return response()->json([
+                'status' => 'Error',
+                'data' => 'Mentor Not Found'
+            ], 404);
+        }
+
+        return response()->json(['status' => 'Success', 'data' => $mentor]);
+    }
+
     public function create(Request $request)
     {
         $rules = [
@@ -31,5 +53,61 @@ class MentorController extends Controller
         $mentor = Mentor::create($data);
 
         return response()->json(['status' => 'Success', 'data' => $mentor]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $rules = [
+            'name' => 'string',
+            'profile' => 'url',
+            'email' => 'email',
+            'profession' => 'string'
+        ];
+
+        $data = $request->all();
+
+        $validator = Validator::make($data, $rules);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'status' => 'Error',
+                'message' => $validator->errors()
+            ], 400);
+        }
+
+        $mentor = Mentor::find($id);
+
+        if(!$mentor)
+        {
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Mentor Not Found'
+            ], 404);
+        }
+
+        $mentor->fill($data);
+        $mentor->save();
+        return response()->json([
+            'status' => 'Success',
+            'data' => $mentor
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $mentor = Mentor::find($id);
+
+        if (!$mentor){
+            return response()->json([
+                'status' => 'Error',
+                'message' => 'Mentor Not Found'
+            ], 404);
+        }
+
+        $mentor->delete();
+        return response()->json([
+            'status' => 'Success',
+            'message' => 'Deleted Mentor Successfully'
+        ]);
     }
 }
--- service-course/routes/api.php
+++ service-course/routes/api.php
@@ -13,5 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::get('mentors', 'App\Http\Controllers\MentorController@index');
+Route::get('mentors/{id}', 'App\Http\Controllers\MentorController@show');
 Route::post('mentors', 'App\Http\Controllers\MentorController@create');
+Route::put('mentors/{id}', 'App\Http\Controllers\MentorController@update');
+Route::delete('mentors/{id}', 'App\Http\Controllers\MentorController@destroy');
